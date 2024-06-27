3d8477380a88eef62150b7b75f99697d776bdee8
vish <mvishnurana@gmail.com>
2024-06-27 12:57:15 +0800

    prepared for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/covid-tracker/',
+  base: './',
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'build',
     rollupOptions: {
       output: {
         manualChunks: {
